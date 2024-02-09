73dd8d2d05e078064cf3cba5f3ea327fb967edd6
Shweta0009 <shwetarai052004@gmail.com>
2024-02-09 23:10:09 +0530

    modifiedSerchPageHomePage

--- src/Pages/homePage.java
+++ src/Pages/homePage.java
@@ -4,5 +4,6 @@ public class homePage {
 	public static void main(String[] args) {
 		System.out.println("this is home page...");
 		int item=40;
+		String city="lucknow";
 	}
 }
--- src/tests/SearchPageTest.java
+++ src/tests/SearchPageTest.java
@@ -3,6 +3,8 @@ package tests;
 public class SearchPageTest {
 	public static void main(String[] args) {
 		System.out.println("search page test");
+		String s="t-shert";
+		String s="tops";
 	}
 
 }
